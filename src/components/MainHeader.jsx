f5e22eb957356fa960bb8b523395066285ddeb5d
frankzeal33 <93032385+frankzeal33@users.noreply.github.com>
2024-03-03 00:26:39 +0100

    Update MainHeader.jsx

--- src/components/MainHeader.jsx
+++ src/components/MainHeader.jsx
@@ -7,7 +7,7 @@ const MainHeader = () => {
       <div className="container main_header-container">
         <div className="main_header-left">
           <h4>#100DaysOfWorkOut</h4>
-          <h1>Join The Legends Of The Fitness World</h1>
+          <h1>It's Your Workout: Your Time. Your Body. Own It.</h1>
           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem temporibus, delectus nemo ea ab id.</p>
           <Link to="/plans" className='btn lg'>Get Started</Link>
         </div>
